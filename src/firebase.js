594ccb6ac0780290881496bc42b05fc67040e57c
Gokarla Srinivas <87422030+srinivasGokarla@users.noreply.github.com>
2025-08-12 16:55:48 +0530

    fixing deployement link issue

--- src/firebase.js
+++ src/firebase.js
@@ -10,8 +10,9 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_MESASUREMENT_ID
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+console.log("Firebase Config:", firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
